1d34bba249cc966eb1ae7d2c36f4c9b116d65376
docente05 <jhopes.p@gmail.com>
2015-09-03 11:25:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/docente05/app_list_activity/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.docente05.app_list_activity;
+
+import android.app.ListActivity;
+import android.database.MatrixCursor;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        //Simulamos que extraemos los datos de la base de datos a un cursor
+        String[] columnasBD = new String[] {"_id", "imagen", "textoSuperior", "textoInferior"};
+        MatrixCursor cursor = new MatrixCursor(columnasBD);
+        cursor.addRow(new Object[] {"0", R.drawable.abc_btn_check_material, "BUHO", "Búho es el nombre común de aves de la familia Strigidae, del orden de las estrigiformes o aves rapaces nocturnas. Habitualmente designa especies que, a diferencia de las lechuzas, tienen plumas alzadas que parecen orejas."});
+        cursor.addRow(new Object[] {"1", R.drawable.abc_btn_check_material, "BUHO", "frffff es el nombre común de aves de la familia Strigidae, del orden de las estrigiformes o aves rapaces nocturnas. Habitualmente designa especies que, a diferencia de las lechuzas, tienen plumas alzadas que parecen orejas."});
+
+        //Añadimos los datos al Adapter y le indicamos donde dibujar cada dato en la fila del Layout
+        String[] desdeEstasColumnas = {"imagen", "textoSuperior", "textoInferior"};
+        int[] aEstasViews = {R.id.imageView_imagen, R.id.textView_superior, R.id.textView_inferior};
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.activity_main, cursor, desdeEstasColumnas, aEstasViews, 0);
+
+        ListView listado = getListView();
+        listado.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
